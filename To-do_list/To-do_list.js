18dab41fdd5180820141fc0a2ed35d26e68ecc6f
Kelvin Tavares Leite <kelvintl89@gmail.com>
2026-05-18 20:12:17 -0300

    Testes
    foram feitos vários testes que infelizmente ainda não trouxeram o resultado esperado.

---
+++ To-do_list/To-do_list.js
@@ -0,0 +1,9 @@
+function CriarTarefa() {
+    let btn = document.getElementById('btnTarefa');
+    let viewT = document.getElementById('btnVisualizar')
+    window.alert('Funcionando');
+    btn.addEventListener("click", () => {
+        caixa.style.display = "block";
+      });
+      btn.innerHTML=""
+}   
